22544e241a008459463470a2d463276937655289
Adelfjj <adelsonfranciscojj@outlook.com>
2023-03-15 21:13:18 -0300

    style and feat: tela de cadastro e função para mostrar ou ocultar tela de cadastro

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const submitBtn = document.querySelector("#submit");
+const createAccount = document.querySelector(".create");
+const information = document.querySelector(".information");
+const ReturnSignIn = document.querySelector(".sign_in");
+let h2 = information.querySelector("h2");
 
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
 });
+
+createAccount.addEventListener("click", () => {
+    information.classList.toggle("sign");
+        h2.textContent = "Join Us!";
+});
+
+ReturnSignIn.addEventListener("click", () => {
+    information.classList.toggle("sign");
+    h2.textContent = "Welcome Back!";
+});
+
